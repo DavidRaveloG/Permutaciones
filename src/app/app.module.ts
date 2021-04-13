85b7d9aa9cd8befefd115067c835a9cdf97dadc4
Your Name <you@example.com>
2021-04-12 19:44:19 -0500

    combinatoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NadvarComponent } from './nadvar/nadvar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NadvarComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+  cantidad_x = 0;
+  cantidad_n = 0;
+  cantidad_x_n = 0;
+  combinatoria = 0;
+  fact: number = 1;
+  cantidad_nF = 0;
+  cantidad_xF = 0;
+  cantidad_xnF = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  combinatoriaC () : void{
+ 
+    this.cantidad_nF = 0;
+    this.combinatoria =0;
+  this.cantidad_xF = 0;
+  this.cantidad_xnF = 0;
+  this.cantidad_x_n = 0;
+  this.cantidad_x_n = this.cantidad_n-this.cantidad_x;
+  this.cantidad_nF=this.factorial(this.cantidad_n);
+  this.cantidad_xF=this.factorial(this.cantidad_x);
+  this.cantidad_xnF = this.cantidad_x*this.factorial(this.cantidad_x_n);
+  this.combinatoria=this.cantidad_nF/(this.cantidad_xnF);
+  }
+  factorial(n: number) :number {
+    this.fact =1;
+    while (n > 0) {
+      this.fact = this.fact * n;
+      n = n - 1;
+    }
+    return this.fact;
+  }
+  
+}
